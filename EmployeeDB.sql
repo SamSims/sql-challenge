f536a246e6381027cf4189a54f202107fb1c45ab
SamSims <sam.sims.13@gmail.com>
2024-11-02 18:32:40 -0500

    finished queries and changed ERD to include correct types

--- EmployeeDB.sql
+++ EmployeeDB.sql
@@ -1,7 +1,7 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
+drop table if exists "Departments";
 CREATE TABLE "Departments" (
     "dept_no" char(4)   NOT NULL,
     "dept_name" varchar(20)   NOT NULL,
@@ -10,6 +10,7 @@ CREATE TABLE "Departments" (
      )
 );
 
+drop table if exists "Employees";
 CREATE TABLE "Employees" (
     "emp_no" int   NOT NULL,
     "emp_title_id" char(5)   NOT NULL,
@@ -23,6 +24,7 @@ CREATE TABLE "Employees" (
      )
 );
 
+drop table if exists "Titles";
 CREATE TABLE "Titles" (
     "title_id" char(5)   NOT NULL,
     "title" varchar(20)   NOT NULL,
@@ -31,6 +33,7 @@ CREATE TABLE "Titles" (
      )
 );
 
+drop table if exists "Salaries";
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
     "salary" int   NOT NULL,
@@ -39,18 +42,20 @@ CREATE TABLE "Salaries" (
      )
 );
 
+drop table if exists "DeptEmp";
 CREATE TABLE "DeptEmp" (
     "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL,
+    "dept_no" char(4)   NOT NULL,
     CONSTRAINT "pk_DeptEmp" PRIMARY KEY (
         "emp_no","dept_no"
      )
 );
 
+drop table if exists "DeptManager";
 CREATE TABLE "DeptManager" (
-    "dept_no" int   NOT NULL,
+    "dept_no" char(4)   NOT NULL,
     "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_DeptEmp" PRIMARY KEY (
+    CONSTRAINT "pk_DeptManager" PRIMARY KEY (
         "dept_no","emp_no"
      )
 );
@@ -75,48 +80,52 @@ REFERENCES "Employees" ("emp_no");
 
 --List the employee number, last name, first name, sex, and salary of each employee 
 select e.emp_no,e.first_name,e.last_name,e.sex,s.salary
-from Employees as e
-join Salaries as s 
-on e.emp_no, s.emp_no;
+from "Employees" as e
+join "Salaries" as s 
+on e.emp_no= s.emp_no;
 
 --List the first name, last name, and hire date for the employees who were hired in 1986
 select first_name, last_name, hire_date
-from Employees
-where hire_date = %1986;
+from "Employees"
+where hire_date between '1/1/1986' and '12/31/1986';
 
 --List the manager of each department along with their 
 --department number, department name, employee number, last name, and first name
 select dm.dept_no, d.dept_name, e.emp_no, e.first_name,e.last_name
-from DeptManager as dm
-join Departments as d, Employees as e
-on dm.dept_no, d.dept_no dm.emp_no, e.empt; 
+from "DeptManager" as dm
+join "Departments" as d on dm.dept_no= d.dept_no
+join "Employees" as e
+on dm.emp_no = e.emp_no; 
 
 --List the department number for each employee along with that employee’s 
 --employee number, last name, first name, and department name
 select d.dept_no, e.emp_no, e.last_name, e.first_name, d.dept_name
-from Employees as e
-join DeptEmp as de, Departments as d
-on e.emp_no, de.emp_no, de.dept_no, d.dept_no;
+from "Employees" as e
+join "DeptEmp" as de on e.emp_no= de.emp_no
+join "Departments" as d
+on de.dept_no= d.dept_no;
 
 --List first name, last name, and sex of each employee whose 
 --first name is Hercules and whose last name begins with the letter B 
 select first_name, last_name, sex
-from Employees
-where first_name = 'Hercules' and last_name = 'B%';
+from "Employees"
+where first_name = 'Hercules' and last_name like 'B%';
 
 --List each employee in the Sales department, including their employee number, last name, and first name
 select emp_no, last_name, first_name
-from Employees
-where emp_no in (select emp_no from DeptEmp where dept_no in (select dept_no from Departments where dept_name='Sales'));
+from "Employees"
+where emp_no in (select emp_no from "DeptEmp" where dept_no in (select dept_no from "Departments" where dept_name='Sales'));
 
 --List each employee in the Sales and Development departments, 
 --including their employee number, last name, first name, and department name
-select e.emp_no, e.last_name, e.first_name, d.dept_name from DeptEmp
-join Employees as e, DeptEmp as de, Departments as d
-on e.emp_no, de.emp_no, de.dept_no, d.dept_no
-where dept_no in(select dept_no from Departments where dept_name in('Sales','Development'));
+select e.emp_no, e.last_name, e.first_name, d.dept_name from "DeptEmp" as de
+join "Employees" as e on e.emp_no= de.emp_no
+join "Departments" as d
+on de.dept_no= d.dept_no
+where de.dept_no in(select dept_no from "Departments" where dept_name in('Sales','Development'));
 
 --List the frequency counts, in descending order, of all the employee last names 
 --(that is, how many employees share each last name)
-select last_name, count(last_name) from Employees
-list descending; 
+select last_name, count(last_name) from "Employees"
+group by last_name
+order by count(last_name) desc; 
